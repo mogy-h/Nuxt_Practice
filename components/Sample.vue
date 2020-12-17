ffda0ccda91a2b9c4b3fcefb819ca7000c125572
mogy-h <rapter.mg@gmail.com>
2020-12-17 13:04:06 +0900

    first commit

---
+++ components/Sample.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h3>Hello, {{ name }}</h3>
+    <p>
+      {{ reverseName }}
+    </p>
+
+    <p :class="['hide', { seven: num === 7 }]">7回クリックしたら表示</p>
+
+    <button type="button" name="button" @click="plus">クリック{{ num }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'mogihiroki',
+      num: 0,
+    }
+  },
+  computed: {
+    reverseName() {
+      return this.name.split('').reverse().join('')
+    },
+  },
+  methods: {
+    plus() {
+      this.num++
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.hide {
+  display: none;
+  &.seven {
+    display: block;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <sample></sample>
+  </div>
+</template>
+
+<script>
+import Sample from '@/components/Sample.vue'
+
+export default {
+  components: {
+    Sample,
+  },
+}
+</script>
+
+<style></style>
